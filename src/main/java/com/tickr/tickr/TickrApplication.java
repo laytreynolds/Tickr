f4db692b31586bf6f7f865037c351b4178208755
Layton Reynolds <laytreynolds@hotmail.com>
2026-02-04 11:32:22 +0000

    intiial commit

---
+++ src/main/java/com/tickr/tickr/TickrApplication.java
@@ -0,0 +1,13 @@
+package com.tickr.tickr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickr/tickr/TickrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickr.tickr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
